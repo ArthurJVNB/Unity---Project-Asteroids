6cee0ab6b0371f63500f5e796ee9adf3472ede7c
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2025-02-15 15:34:45 -0300

    Started implementation of Spaceship script

--- Assets/Features/Game/Spaceship.cs
+++ Assets/Features/Game/Spaceship.cs
@@ -1,9 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Project
 {
 	public class Spaceship : MonoBehaviour
 	{
+		[SerializeField] private Rigidbody2D _rigidbody;
+		[SerializeField] private float _thrustSpeed = 1;
 
+		bool _isThrusting;
+
+		private void Reset()
+		{
+			_rigidbody = GetComponentInChildren<Rigidbody2D>();
+		}
+
+		public void OnMove(InputValue value)
+		{
+			Debug.Log("OnMove " + value.Get<Vector2>());
+			_isThrusting = value.Get<Vector2>() != Vector2.zero;
+		}
+
+		public void Thrust()
+		{
+			_isThrusting = true;
+		}
+
+		private void FixedUpdate()
+		{
+			if (_isThrusting)
+			{
+
+			}
+
+		}
 	}
 }
