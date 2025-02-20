dc5ae0547c4529ccafb85369ccd394b12c1ce79e
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2025-02-19 21:34:20 -0300

    Basic implementation of Asteroid (following a video)

--- Assets/Features/Game/Asteroid.cs
+++ Assets/Features/Game/Asteroid.cs
@@ -6,7 +6,11 @@ namespace Project
 	{
 		[SerializeField] private SpriteRenderer _spriteRenderer;
 		[SerializeField] private Rigidbody2D _rigidbody;
+		[Space]
 		[SerializeField] private Sprite[] _sprites;
+		[SerializeField] private float _size = 1;
+		[SerializeField] private float _minSize = 0.5f;
+		[SerializeField] private float _maxSize = 1.5f;
 
 		private void Reset()
 		{
@@ -14,6 +18,14 @@ namespace Project
 			_rigidbody = GetComponentInChildren<Rigidbody2D>();
 		}
 
+		private void Start()
+		{
+			_spriteRenderer.sprite = _sprites[Random.Range(0, _sprites.Length)];
 
+			transform.eulerAngles = new Vector3(0, 0, Random.Range(0, 360));
+			transform.localScale = Vector3.one * _size;
+
+			_rigidbody.mass = _size;
+		}
 	}
 }
