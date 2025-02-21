4fc43105375850c041c49c047560078fb53acbc3
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2025-02-20 23:57:44 -0300

    Implemented IntEventData and created scriptable object called "LifesChangedIntEventData"

--- Assets/Features/_Core/EventBus/AEventDataT.cs
+++ Assets/Features/_Core/EventBus/AEventDataT.cs
@@ -5,6 +5,8 @@ namespace Project
 {
 	public abstract class AEventDataT<T> : ScriptableObject
 	{
+		protected const string ScriptablePath = "Scriptable Objects/Event Bus/";
+
 		public event Action<T> Event = _ => { };
 
 		public void Invoke(T item) => Event.Invoke(item);
---
+++ Assets/Features/_Core/EventBus/IntEventData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace Project
+{
+	[CreateAssetMenu(fileName = "IntEventData", menuName = ScriptablePath + "Int Event")]
+	public class IntEventData : AEventDataT<int> { }
+}
