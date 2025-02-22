898a12bf29cc493691fbfc3f64d580aa6404918f
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2025-02-22 03:16:20 -0300

    Implemented Collision2DEventData and created some scriptables of events related to it (ranging from various types of collisions in the game)

---
+++ Assets/Features/_Core/EventBus/Collision2DEventData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace Project
+{
+	[CreateAssetMenu(fileName = "Collision2DEventData", menuName = ScriptablePath + "Collision2D Event")]
+	public class Collision2DEventData : AEventDataT<Collision2D> { }
+}
