2cafabd7a73232e39d67776863b333ff789dd13a
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2025-03-19 20:54:59 -0300

    Implemented Shooter to play sound when shooting (using AudioDataArray)

--- Assets/Features/Game/Shooter.cs
+++ Assets/Features/Game/Shooter.cs
@@ -1,3 +1,4 @@
+using Project.Sound;
 using UnityEngine;
 
 namespace Project
@@ -6,6 +7,11 @@ namespace Project
 	{
 		[SerializeField] private Transform _positionToShoot;
 		[SerializeField] private Bullet _prefab;
+		[Tooltip("Optional. If set, it will play the audio data on shoot")]
+		[SerializeField] private AudioDataArray _shootAudioDataArray;
+
+		[Header("Events Invoked")]
+		[SerializeField] private Vector2EventData _onShootEvent;
 
 		private void Reset()
 		{
@@ -21,6 +27,8 @@ namespace Project
 		{
 			var item = Instantiate(_prefab, _positionToShoot.position, _positionToShoot.rotation);
 			item.Project(_positionToShoot.up);
+			if (_shootAudioDataArray) _shootAudioDataArray.PlayOneShot(item.transform.position);
+			_onShootEvent.Invoke(item.transform.position);
 		}
 	}
 }
