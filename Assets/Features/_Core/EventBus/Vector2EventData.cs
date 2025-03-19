371a407291d016662825850670a0a66ad382ddd6
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2025-03-19 20:53:58 -0300

    Implemented Vector2EventData and created an event to be used when a shot happens

---
+++ Assets/Features/_Core/EventBus/Vector2EventData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace Project
+{
+	[CreateAssetMenu(fileName = "Vector2EventData", menuName = ScriptablePath + "Vector2 Event")]
+	public class Vector2EventData : AEventDataT<Vector2> { }
+}
